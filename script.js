c248b85d9b9a1e85e5ea0eee8c0ed4f774a28c4d
ISADORA SOZO DA SILVA <isadora_sozo-silva@estudante.sesisenai.org.br>
2025-04-02 09:54:00 -0300

    criando rotas e um servidor

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const http = require('http');
+const url = require('url')
 const {soma} = require('./teste')
 
 const PORT = 8000
 
 //criar o servidor
 const server = http.createServer((req, res) => {
-    let valores = req.url
-        .replace('/','')
-        .replace('?','')
-        .split('&')
-    let parametros = {}
-    for (item in valores){
-        let chaveValor = valores[item].split('=')
-        parametros[chaveValor[0]] = Number(chaveValor[1])
+    const urlCapturada = url.parse(req.url, true)
+    const {query, pathname} = urlCapturada
+    let resultado = 0
+    let mensagem = `O resultado é ${resultado}`
+if (pathname == '/soma'){
+    resultado  = soma (Number(query.a), Number(query.b))
+}else if (pathname == '/subtracao'){ 
+    resultado = subtracao (Number(query.a), Number(query.b))
+}else if (pathname == '/multi'){ 
+    resultado = multi (Number(query.a), Number(query.b))
+}else if (pathname == '/divisao'){ 
+    resultado = divisao (Number(query.a), Number(query.b))
+} else{
+    res.statusCode = 404
+    mensagem = 'Página não encontrada'
+}
 
-    }
-    console.log(parametros)
-    let resultado = soma (parametros.a, parametros.b)
-    res.end(`O valor final é ${resultado}`)
+
+res.end(`O valor final é ${resultado}`)
 })
 //fazer ele ouvir na porta 8000
 server.listen(PORT, () => {
